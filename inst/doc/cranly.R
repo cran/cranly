bab5870827e2a74ea0dc73df82e4c89b41fb70e5
Ioannis Kosmidis <ioannis.kosmidis@warwick.ac.uk>
2022-08-26 21:14:41 +0000

    version 0.6.0

--- inst/doc/cranly.R
+++ inst/doc/cranly.R
@@ -1,4 +1,4 @@
-## ----setup, include = FALSE----------------------------------------------
+## ----setup, include = FALSE---------------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>",
@@ -6,57 +6,59 @@ knitr::opts_chunk$set(
   fig.height = 6
 )
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 library("cranly")
-p_db <- tools::CRAN_package_db()
 
-## ------------------------------------------------------------------------
-package_db <- clean_CRAN_db(p_db)
+## -----------------------------------------------------------------------------
+cran_db <- readRDS(url("https://raw.githubusercontent.com/ikosmidis/cranly/develop/inst/extdata/cran_db.rds"))
 
-## ------------------------------------------------------------------------
-attr(package_db, "timestamp")
+## ----eval = FALSE-------------------------------------------------------------
+#  p_db <- tools::CRAN_package_db()
 
-## ------------------------------------------------------------------------
-package_network <- build_network(package_db)
+## ----eval = FALSE-------------------------------------------------------------
+#  cran_db <- clean_CRAN_db(p_db)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
+attr(cran_db, "timestamp")
+
+## -----------------------------------------------------------------------------
+package_network <- build_network(cran_db)
+
+## -----------------------------------------------------------------------------
 ## Global package network statistics
 package_summaries <- summary(package_network)
 
-## ------------------------------------------------------------------------
-plot(package_summaries, according_to = "n_imported_by", top = 20)
-plot(package_summaries, according_to = "page_rank", top = 20)
-plot(package_summaries, according_to = "betweenness", top = 20)
-plot(package_summaries, according_to = "n_enhances", top = 20)
+## -----------------------------------------------------------------------------
 plot(package_summaries, according_to = "n_authors", top = 20)
 plot(package_summaries, according_to = "n_imports", top = 20)
+plot(package_summaries, according_to = "n_imported_by", top = 20)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
+names(package_summaries)
+
+## -----------------------------------------------------------------------------
 my_packages <- package_by(package_network, "Ioannis Kosmidis")
 my_packages
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 plot(package_network, package = my_packages, title = TRUE, legend = TRUE)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 optional_packages <- subset(package_network, recommended = FALSE, base = FALSE)
 optional_summary <- summary(optional_packages)
 plot(optional_summary, top = 30, according_to = "n_imported_by")
 
-## ------------------------------------------------------------------------
-author_network <- build_network(object = package_db, perspective = "author")
+## -----------------------------------------------------------------------------
+author_network <- build_network(object = cran_db, perspective = "author")
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 author_summaries <- summary(author_network)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 plot(author_summaries, according_to = "n_packages", top = 20)
 plot(author_summaries, according_to = "page_rank", top = 20)
 plot(author_summaries, according_to = "betweenness", top = 20)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 plot(author_network, author = "R Core")
-
-## ------------------------------------------------------------------------
-plot(author_network, author = "Kosmidis", exact = FALSE)
 
--- inst/doc/dependence_trees.R
+++ inst/doc/dependence_trees.R
@@ -1,4 +1,4 @@
-## ----setup, include = FALSE----------------------------------------------
+## ----setup, include = FALSE---------------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>",
@@ -6,22 +6,27 @@ knitr::opts_chunk$set(
   fig.height = 6
 )
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 library("cranly")
-cran_db <- clean_CRAN_db()
-package_network <- build_network(cran_db, perspective = "package")
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
+package_network <- readRDS(url("https://raw.githubusercontent.com/ikosmidis/cranly/develop/inst/extdata/package_network.rds"))
+
+## ----eval = FALSE-------------------------------------------------------------
+#  cran_db <- clean_CRAN_db()
+#  package_network <- build_network(cran_db)
+
+## -----------------------------------------------------------------------------
  compute_dependence_tree(package_network, "brglm2")
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 compute_dependence_tree(package_network, "tibble")
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 tibble_tree <- build_dependence_tree(package_network, "tibble")
 plot(tibble_tree)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 betareg_tree <- build_dependence_tree(package_network, "betareg")
 betareg_dep_index <- sapply(betareg_tree$nodes$package, function(package) {
     tree <- build_dependence_tree(package_network, package = package)
